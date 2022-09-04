22bb30b0080ad5a0d6dd115013b24a164a1c18a4
Adrian Cruz <adr.ca@live.com>
2022-09-04 17:56:59 -0400

    add theme-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
+import { Box, Heading, Button } from 'theme-ui'
+
 import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
+    <Box m={20} mt={40} p={15}>
       <div>
         <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
           <img src='/vite.svg' className='logo' alt='Vite logo' />
@@ -15,11 +16,11 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <Heading>Vite + React</Heading>
       <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button>
+        </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -27,7 +28,7 @@ function App() {
       <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
-    </div>
+    </Box>
   )
 }
 
