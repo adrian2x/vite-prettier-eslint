9e4170e64b835574ba06a0bc2b50aa84d2782082
Adrian Cruz <adr.ca@live.com>
2022-09-05 11:48:31 -0400

    @carbon/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Button } from '@carbon/react'
 
 function App() {
   return (
     <div>
       <h1>This is a headline</h1>
-      <button>Get started</button>
+      <Button>Get started</Button>
     </div>
   )
 }
